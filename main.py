e8b4d3cb9413f663067dcbe82a21cc997c488625
Navin319931 <navinsharma9376319931@gmail.com>
2023-10-27 10:49:22 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # You can create an HTML template for your web page
+
+@socketio.on('message')
+def handle_message(message):
+    print(f'Received message: {message}')
+    socketio.emit('response', message)  # Send the message back to the client
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
